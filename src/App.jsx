04c7e954c3853d0b74ac667af267d57035058638
Gympiecuddles <schembri.y93@gmail.com>
2022-08-16 14:02:51 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Eeeedit <code>src/App.jsx</code> and save to test HMR
+          Eedit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">
